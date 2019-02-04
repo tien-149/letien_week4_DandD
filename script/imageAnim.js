76c727c282ea53febc1edd7671738ee78f16b06f
Le <t_le86524@tss-hr420-dm22.fc.ca>
2019-02-04 16:40:29 -0500

    Work of week 5

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -2,21 +2,52 @@
 
 	//sete up puzzle piece and boards
 	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
-
+	// This is a string
 	let piecesBoard = document.querySelector(".puzzle-pieces"),
 		puzzleBoard = document.querySelector('.puzzle-board'),
 		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+
+	let dropZones = document.querySelectorAll('.drop-zone')
 	// function go in the middle
 	function createPuzzlePieces(pictureIndex) {
 		//generate puzzle piece for the left hand side
 		// debugger;
 		pieces.forEach((piece, index) => {
-			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+			let newPuzzlePiece = `<img draggable id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
 			piecesBoard.innerHTML += newPuzzlePiece;
 		});
 
 		puzzleBoard.style.backgroundImage = `url(./images/backGround${pictureIndex}.jpg)`
+
+		initDrag();
 	}
+
+	//Drag and drop funtionality goes here
+	function initDrag() {
+		piecesBoard.querySelectorAll('img').forEach(img => {
+			img.addEventListener("dragstart", function(e) {
+				//e.preventDefault();
+				console.log('draggin...');
+
+				e.dataTransfer.setData("text/plain", this.id);
+			});
+		});
+	}
+
+	//handle dragover and drop
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			console.log("You dragged over me");
+		});
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault();
+			console.log("You drop some sumpin on me");
+
+			let piece = e.dataTransfer.getData("text/plain");
+			e.target.appendChild(document.querySelector(`#${piece}`));
+		});
+	});
 
 	function resetPuzzlePieces() {
 		// empty the thumnail container
